8a1faf5b0fdf54473f7cbebd3f4b379b845860ec
Semih Tok <ibrahim@semihtok.com>
2024-08-30 16:40:23 +0300

    Update lsm.rb

--- lsm.rb
+++ lsm.rb
@@ -5,14 +5,14 @@
 class Lsm < Formula
   desc "lsm (kube ls) is a Kubernetes tool that allows you to list Kubernetes resources with a simple command(s)."
   homepage "https://github.com/semihtok/lsm"
-  version "0.2.1"
+  version "0.1.2"
   license "Apache 2.0"
 
   depends_on "git"
   depends_on "go"
 
   on_macos do
-    url "https://github.com/semihtok/lsm/releases/download/v0.2.1/lsm_0.2.1_darwin_amd64.tar.gz"
+    url "https://github.com/semihtok/lsm/releases/download/v0.1.2/lsm_0.1.2_darwin_amd64.tar.gz"
     sha256 "30f797846626f7419a5fe82eea304190c603008630b546ac9fe3c1709294e4ac"
 
     def install
@@ -32,7 +32,7 @@ class Lsm < Formula
 
   on_linux do
     if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-      url "https://github.com/semihtok/lsm/releases/download/v0.2.1/lsm_0.2.1_linux_armv6.tar.gz"
+      url "https://github.com/semihtok/lsm/releases/download/v0.1.2/lsm_0.1.2_linux_armv6.tar.gz"
       sha256 "839c0451605c90ad0a221384cd7cb797990a35305620a375fcb42628ce51fdb4"
 
       def install
@@ -40,7 +40,7 @@ class Lsm < Formula
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/semihtok/lsm/releases/download/v0.2.1/lsm_0.2.1_linux_amd64.tar.gz"
+      url "https://github.com/semihtok/lsm/releases/download/v0.1.2/lsm_0.1.2_linux_amd64.tar.gz"
       sha256 "2fba15bace295e3a62070921762a8f05bc417695864e47cedd82a744bf10894b"
 
       def install
